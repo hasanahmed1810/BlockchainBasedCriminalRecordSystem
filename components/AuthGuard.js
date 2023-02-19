4fd18e5b871cad2ad6acc23b0d08ce5d79b014bf
Hasan Ahmed <hasan.ahmed1810@gmail.com>
2023-02-20 04:45:40 +0500

    added auth guard

---
+++ components/AuthGuard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useEffect } from "react";
+import { auth } from "../firebase";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useRouter } from "next/router";
+
+function AuthGuard({ children }) {
+  const router = useRouter();
+
+  function signout() {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  }
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        router.push("/login");
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <button
+        onClick={signout}
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 m-8  focus:outline-none"
+      >
+        sign out
+      </button>
+      {children}
+    </div>
+  );
+}
+
+export default AuthGuard;
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import "../styles/globals.css";
+import AuthGuard from "../components/AuthGuard";
 // import NoSSR from "react-no-ssr";
 
 function MyApp({ Component, pageProps }) {
   return (
+    <AuthGuard>
       <Component {...pageProps} />
+    </AuthGuard>
   );
 }
 
