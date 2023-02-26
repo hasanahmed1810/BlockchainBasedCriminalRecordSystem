7b4d4bec7556b1e53e705b76dbc6ac13099a6af0
Hasan Ahmed <hasan.ahmed1810@gmail.com>
2023-02-26 07:32:08 +0500

    remove comment

--- pages/success.js
+++ pages/success.js
@@ -15,12 +15,6 @@ function success() {
   return (
     <div class="bg-white h-screen">
       <div class=" p-6  md:mx-auto">
-        {/* <svg viewBox="0 0 24 24" class="text-green-600 w-16 h-16 mx-auto my-6">
-          <path
-            fill="currentColor"
-            d="M12,0A12,12,0,1,0,24,12,12.014,12.014,0,0,0,12,0Zm6.927,8.2-6.845,9.289a1.011,1.011,0,0,1-1.43.188L5.764,13.769a1,1,0,1,1,1.25-1.562l4.076,3.261,6.227-8.451A1,1,0,1,1,18.927,8.2Z"
-          ></path>
-        </svg> */}
         <Lottie options={defaultOptions} height={400} width={400} />
         <div class="text-center">
           <h1 class="md:text-2xl text-base text-gray-900 font-semibold text-center">
