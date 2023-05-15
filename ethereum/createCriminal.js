20676d57bc837909f722b6c0ef2be5459e0e88ff
Hasan Ahmed <hasan.ahmed1810@gmail.com>
2023-05-16 00:17:07 +0500

    changed address

--- ethereum/createCriminal.js
+++ ethereum/createCriminal.js
@@ -3,7 +3,7 @@ import CreateCriminal from "../artifacts/CreateCriminal_metadata.json";
 
 const instance = new web3.eth.Contract(
   CreateCriminal.output.abi,
-  '0xB145cd2a292F10a3EdaCb87a36Cf7665E4f23da3'
+  '0x02F622066B015972D1b8AFE9C4b4e4E4f3958419'
 );
 
 export default instance;
